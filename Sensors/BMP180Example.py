cd94dbe327e6f4a0e9aa51f0af58ef46e112f601
Max Huggins <max.huggins98@gmail.com>
2021-01-02 11:41:02 -0600

    Adding BMP files from Adafruit.

---
+++ Sensors/BMP180Example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  2 11:38:38 2021
+
+@author: max
+"""
+
+
+import BMP085 as BMP085
+
+sensor = BMP085.BMP085()
+
+
+print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+print('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
+print('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
+print('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
